d09f5412448c2d8992d0bef7a3157475e59b4e1a
hmnisreen <nisshmt@gmail.com>
2024-05-11 10:15:08 +0100

    db connection

---
+++ db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database',{
+    userNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
